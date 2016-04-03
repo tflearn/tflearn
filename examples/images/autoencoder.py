f78beb38c98e1048004355f69ceb7f3cf9ca28a9
aymericdamien <aymeric.damien@gmail.com>
2016-04-03 23:03:14 +0800

    added auto encoder example

---
+++ examples/images/autoencoder.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+""" Auto Encoder Example.
+
+Using an auto encoder on MNIST handwritten digits.
+
+References:
+    Y. LeCun, L. Bottou, Y. Bengio, and P. Haffner. "Gradient-based
+    learning applied to document recognition." Proceedings of the IEEE,
+    86(11):2278-2324, November 1998.
+
+Links:
+    [MNIST Dataset] http://yann.lecun.com/exdb/mnist/
+
+"""
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tflearn
+
+# Data loading and preprocessing
+import tflearn.datasets.mnist as mnist
+X, Y, testX, testY = mnist.load_data(one_hot=True)
+
+# Building the encoder
+encoder = tflearn.input_data(shape=[None, 784])
+encoder = tflearn.fully_connected(encoder, 256)
+encoder = tflearn.fully_connected(encoder, 64)
+
+# Building the decoder
+decoder = tflearn.fully_connected(encoder, 256)
+decoder = tflearn.fully_connected(decoder, 784)
+
+# Regression, with mean square error between
+net = tflearn.regression(decoder, optimizer='adam', learning_rate=0.001,
+                         loss='mean_square', metric=None)
+
+# Training the auto encoder
+model = tflearn.DNN(net, tensorboard_verbose=0)
+model.fit(X, X, n_epoch=10, validation_set=(testX, testX),
+          run_id="auto_encoder", batch_size=256)
+
+# Encoding X[0] for test
+print("\nTest encoding of X[0]:")
+encoding_model = tflearn.DNN(encoder)
+print(encoding_model.predict([X[0]]))
+
+# Testing the image reconstruction on new data (test set)
+print("\nVisualizing results after being encoded and decoded:")
+testX = tflearn.data_utils.shuffle(testX)[0]
+# Applying encode and decode over test set
+encode_decode = model.predict(testX)
+# Compare original images with their reconstructions
+f, a = plt.subplots(2, 10, figsize=(10, 2))
+for i in range(10):
+    a[0][i].imshow(np.reshape(testX[i], (28, 28)))
+    a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
+f.show()
+plt.draw()
+plt.waitforbuttonpress()
