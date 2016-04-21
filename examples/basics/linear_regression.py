3f89cb12bb86f7273054a1c3c1564aadbb4256ea
Aziz Alto <iamaziz.alto@gmail.com>
2016-04-20 21:10:01 -0400

    change a built-in variable name.

--- examples/basics/linear_regression.py
+++ examples/basics/linear_regression.py
@@ -9,8 +9,8 @@ X = [3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997
 Y = [1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3]
 
 # Linear Regression graph
-input = tflearn.input_data(shape=[None])
-linear = tflearn.single_unit(input)
+input_ = tflearn.input_data(shape=[None])
+linear = tflearn.single_unit(input_)
 regression = tflearn.regression(linear, optimizer='sgd', loss='mean_square',
                                 metric='R2', learning_rate=0.01)
 m = tflearn.DNN(regression)
