0ed1a2182f27718dc403507d371043e31b4ca23a
Donghoon Lee <hoondy@users.noreply.github.com>
2016-05-20 11:33:48 -0400

    Update lstm_generator_cityname.py
    1. SSL issue with URL call
    2. Convert city names to lower case

--- examples/nlp/lstm_generator_cityname.py
+++ examples/nlp/lstm_generator_cityname.py
@@ -1,19 +1,21 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-from six.moves import urllib
+from six import moves
+import ssl
 
 import tflearn
 from tflearn.data_utils import *
 
 path = "US_Cities.txt"
 if not os.path.isfile(path):
-    urllib.request.urlretrieve("https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/US_Cities.txt", path)
+    context = ssl._create_unverified_context()
+    moves.urllib.request.urlretrieve("https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/US_Cities.txt", path, context=context)
 
 maxlen = 20
 
 X, Y, char_idx = \
-    textfile_to_semi_redundant_sequences(path, seq_maxlen=maxlen, redun_step=3)
+    textfile_to_semi_redundant_sequences(path, seq_maxlen=maxlen, redun_step=3, to_lower_case=True)
 
 g = tflearn.input_data(shape=[None, maxlen, len(char_idx)])
 g = tflearn.lstm(g, 512, return_seq=True)
