e5d4ea271526bcda046b1c23f8a387cb8f740cb2
aymericdamien <aymeric.damien@gmail.com>
2016-06-07 12:54:59 +0800

    added new initializations

--- tflearn/initializations.py
+++ tflearn/initializations.py
@@ -2,8 +2,9 @@ from __future__ import division, print_function, absolute_import
 
 import math
 import tensorflow as tf
+from tensorflow.contrib.layers.python.layers.initializers import \
+    xavier_initializer, variance_scaling_initializer
 from .utils import get_from_module
-
 
 def get(identifier):
     if hasattr(identifier, '__call__'):
@@ -162,3 +163,80 @@ def truncated_normal(shape=None, mean=0.0, stddev=0.02, dtype=tf.float32,
     else:
         return tf.truncated_normal_initializer(mean=mean, stddev=stddev,
                                                seed=seed, dtype=dtype)
+
+
+def xavier(uniform=True, seed=None, dtype=tf.float32):
+    """ Xavier.
+
+    Returns an initializer performing "Xavier" initialization for weights.
+
+    This initializer is designed to keep the scale of the gradients roughly the
+    same in all layers. In uniform distribution this ends up being the range:
+    `x = sqrt(6. / (in + out)); [-x, x]` and for normal distribution a standard
+    deviation of `sqrt(3. / (in + out))` is used.
+
+    Arguments:
+        uniform: Whether to use uniform or normal distributed random
+            initialization.
+        seed: A Python integer. Used to create random seeds. See
+            `set_random_seed` for behavior.
+        dtype: The data type. Only floating point types are supported.
+
+    Returns:
+        An initializer for a weight matrix.
+
+    References:
+        Xavier Glorot and Yoshua Bengio (2010): Understanding the difficulty of
+        training deep feedforward neural networks. International conference on
+        artificial intelligence and statistics.
+    """
+    return xavier_initializer(uniform=uniform, seed=seed, dtype=dtype)
+
+
+def variance_scaling(factor=2.0, mode='FAN_IN', uniform=False, seed=None,
+                     dtype=tf.float32):
+    """ Variance Scaling.
+
+    Returns an initializer that generates tensors without scaling variance.
+
+    When initializing a deep network, it is in principle advantageous to keep
+    the scale of the input variance constant, so it does not explode or diminish
+    by reaching the final layer. This initializer use the following formula:
+    if mode='FAN_IN': # Count only number of input connections.
+      n = fan_in
+    elif mode='FAN_OUT': # Count only number of output connections.
+      n = fan_out
+    elif mode='FAN_AVG': # Average number of inputs and output connections.
+      n = (fan_in + fan_out)/2.0
+
+      truncated_normal(shape, 0.0, stddev=sqrt(factor / n))
+
+    To get http://arxiv.org/pdf/1502.01852v1.pdf use (Default):
+    - factor=2.0 mode='FAN_IN' uniform=False
+    To get http://arxiv.org/abs/1408.5093 use:
+    - factor=1.0 mode='FAN_IN' uniform=True
+    To get http://jmlr.org/proceedings/papers/v9/glorot10a/glorot10a.pdf use:
+    - factor=1.0 mode='FAN_AVG' uniform=True.
+    To get xavier_initializer use either:
+    - factor=1.0 mode='FAN_AVG' uniform=True.
+    - factor=1.0 mode='FAN_AVG' uniform=False.
+
+    Arguments:
+        factor: Float.  A multiplicative factor.
+        mode: String.  'FAN_IN', 'FAN_OUT', 'FAN_AVG'.
+        uniform: Whether to use uniform or normal distributed random
+            initialization.
+        seed: A Python integer. Used to create random seeds. See
+            `set_random_seed` for behavior.
+        dtype: The data type. Only floating point types are supported.
+
+    Returns:
+    An initializer that generates tensors with unit variance.
+
+    Raises:
+    ValueError: if `dtype` is not a floating point type.
+    TypeError: if `mode` is not in ['FAN_IN', 'FAN_OUT', 'FAN_AVG'].
+    """
+    return variance_scaling_initializer(factor=factor, mode=mode,
+                                        uniform=uniform, seed=seed,
+                                        dtype=dtype)
