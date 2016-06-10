a55c1fdfed6ecc206b9b907b38154889ddb931e7
aymericdamien <aymeric.damien@gmail.com>
2016-06-10 13:49:31 +0800

    release v0.2.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(name='tflearn',
-      version='0.2.0',
+      version='0.2.1',
       description='Deep Learning Library featuring a higher-level API for '
                   'Tensorflow',
       author='TFLearn contributors',
       author_email='aymeric.damien@gmail.com',
       url='https://github.com/tflearn/tflearn',
-      download_url='https://github.com/tflearn/tflearn/tarball/0.2.0',
+      download_url='https://github.com/tflearn/tflearn/tarball/0.2.1',
       license='MIT',
       packages=find_packages(),
       install_requires=[
