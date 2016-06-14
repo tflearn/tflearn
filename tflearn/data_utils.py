5c27bc21df7eee67bd431ce64da3ea02c01272d2
aymericdamien <aymeric.damien@gmail.com>
2016-06-14 16:41:49 +0800

    helper to build hdf5 image dataset
    Added a function to data_utils to automatically build a hdf5 dataset from a folder or file.

--- tflearn/data_utils.py
+++ tflearn/data_utils.py
@@ -18,9 +18,9 @@ data, they are not meant to be use with Tensors or Layers.
 _EPSILON = 1e-8
 
 
-# ------------------------------
-# TARGETS (LABELS) PREPROCESSING
-# ------------------------------
+# =======================
+# TARGETS (LABELS) UTILS
+# =======================
 
 
 def to_categorical(y, nb_classes):
@@ -43,9 +43,9 @@ def to_categorical(y, nb_classes):
     return Y
 
 
-# -----------------------
-# SEQUENCES PREPROCESSING
-# -----------------------
+# =====================
+#    SEQUENCES UTILS
+# =====================
 
 
 def pad_sequences(sequences, maxlen=None, dtype='int32', padding='pre',
@@ -270,9 +270,107 @@ class VocabularyProcessor(_VocabularyProcessor):
         """
         return super(VocabularyProcessor, cls).restore(filename)
 
-# --------------------
-# IMAGES PREPROCESSING
-# --------------------
+
+# ===================
+#    IMAGES UTILS
+# ===================
+
+def build_hdf5_image_dataset(target_path, image_shape, output_path='dataset.h5',
+                             mode='folder', categorical_labels=True,
+                             normalize_img=True, grayscale=False,
+                             files_extension=None, chunks=True):
+    """ Build HDF5 Image Dataset.
+
+    Build an HDF5 dataset by providing either a root folder or a plain text
+    file with images path and class id.
+
+    'folder' mode: Root folder should be arranged as follow:
+    ```
+    ROOT_FOLDER -> SUBFOLDER_0 (CLASS 0) -> CLASS0_IMG1.jpg
+                                         -> CLASS0_IMG2.jpg
+                                         -> ...
+                -> SUBFOLDER_1 (CLASS 1) -> CLASS1_IMG1.jpg
+                                         -> ...
+                -> ...
+    ```
+    Note that if sub-folders are not integers from 0 to n_classes, an id will
+    be assigned to each sub-folder following alphabetical order.
+
+    'file' mode: Plain text file should be formatted as follow:
+    ``
+    /path/to/img1 class_id
+    /path/to/img2 class_id
+    /path/to/img3 class_id
+    ```
+
+    Arguments:
+        target_path: `str`. Path of root folder or images plain text file.
+        image_shape: `tuple (height, width)`. The images shape. Images that
+            doesn't match that shape will be resized.
+        output_path: `str`. The output path for the hdf5 dataset. Default:
+            'dataset.h5'
+        mode: `str` in ['file', 'folder']. The data source mode. 'folder'
+            accepts a root folder with each of his sub-folder representing a
+            class containing the images to classify.
+            'file' accepts a single plain text file that contains every
+            image path with their class id.
+            Default: 'folder'.
+        categorical_labels: `bool`. If True, labels are converted to binary
+            vectors.
+        normalize_img: `bool`. If True, normalize all pictures by dividing
+            every image array by 255.
+        grayscale: `bool`. If true, images are converted to grayscale.
+        files_extension: `list of str`. A list of allowed image file
+            extension, for example ['.jpg', '.jpeg', '.png']. If None,
+            all files are allowed.
+        chunks: `bool` or `list of int`. Whether to chunks the dataset or not.
+            Additionaly, a specific shape for each chunk can be provided.
+
+    """
+    import h5py
+
+    assert image_shape, "Image shape must be defined."
+    assert image_shape[0] and image_shape[1], \
+        "Image shape error. It must be a tuple of int: ('width', 'height')."
+    assert mode in ['folder', 'file'], "`mode` arg must be 'folder' or 'file'"
+
+    if mode == 'folder':
+        images, labels = directory_to_samples(target_path,
+                                              flags=files_extension)
+    else:
+        with open(target_path, 'r') as f:
+            images, labels = [], []
+            for l in f.readlines():
+                l = l.split(' ')
+                images.append(l[0])
+                labels.append(l[1])
+
+    n_classes = np.max(labels) + 1
+
+    d_imgshape = (len(images), image_shape[0], image_shape[1], 3) \
+        if not grayscale else (len(images), image_shape[0], image_shape[1])
+    d_labelshape = (len(images), n_classes) \
+        if categorical_labels else (len(images), )
+
+    dataset = h5py.File(output_path, 'w')
+    dataset.create_dataset('X', d_imgshape, chunks=chunks)
+    dataset.create_dataset('Y', d_labelshape, chunks=chunks)
+
+    for i in range(len(images)):
+        img = load_image(images[i])
+        width, height = img.size
+        if width != image_shape[0] or height != image_shape[1]:
+            img = resize_image(img, image_shape[0], image_shape[1])
+        if grayscale:
+            img = convert_color(img, 'L')
+        img = pil_to_nparray(img)
+        if normalize_img:
+            img /= 255.
+        dataset['X'][i] = img
+        if categorical_labels:
+            dataset['Y'][i] = to_categorical([labels[i]], n_classes)[0]
+        else:
+            dataset['Y'][i] = labels[i]
 
 
 def load_image(in_image):
@@ -362,9 +460,9 @@ def random_flip_updown(x):
         return x
 
 
-# ------------------
-# DATA PREPROCESSING
-# ------------------
+# ==================
+#     DATA UTILS
+# ==================
 
 
 def shuffle(*arrs):
@@ -477,15 +575,15 @@ def directory_to_samples(directory, flags=None):
         c_dir = os.path.join(directory, c)
         for sample in os.walk(c_dir).next()[2]:
             if not flags or any(flag in sample for flag in flags):
-                    samples.append(os.path.join(c_dir, sample))
-                    targets.append(label)
+                samples.append(os.path.join(c_dir, sample))
+                targets.append(label)
         label += 1
     return samples, targets
 
 
-# ------------------
-# OTHERS
-# ------------------
+# ==================
+#    OTHERS
+# ==================
 
 def get_max(X):
     return np.max(X)
