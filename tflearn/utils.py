cb9b37163884565f9ea14d89aef5cd44110e98d6
ranarag <anu15roy@gmail.com>
2016-06-15 21:54:47 +0530

    conv3d feature added (#142)

--- tflearn/utils.py
+++ tflearn/utils.py
@@ -349,9 +349,9 @@ def check_restore_tensor(tensor_to_check, exclvars):
 
 # Auto format kernel
 def autoformat_kernel_2d(strides):
-    if type(strides) is int:
+    if isinstance(strides, int):
         return [1, strides, strides, 1]
-    elif type(strides) in [tuple, list]:
+    elif isinstance(strides, (tuple, list)):
         if len(strides) == 2:
             return [1, strides[0], strides[1], 1]
         elif len(strides) == 4:
@@ -366,9 +366,9 @@ def autoformat_kernel_2d(strides):
 # Auto format filter size
 # Output shape: (rows, cols, input_depth, out_depth)
 def autoformat_filter_conv2d(fsize, in_depth, out_depth):
-    if type(fsize) is int:
+    if isinstance(fsize,int):
         return [fsize, fsize, in_depth, out_depth]
-    elif type(fsize) in [tuple, list]:
+    elif type(fsize, (tuple, list)):
         if len(fsize) == 2:
             return [fsize[0], fsize[1], in_depth, out_depth]
         else:
@@ -384,3 +384,52 @@ def autoformat_padding(padding):
         return str.upper(padding)
     else:
         raise Exception("Unknow padding! Accepted values: 'same', 'valid'.")
+
+
+# Auto format filter size
+# Output shape: (rows, cols, input_depth, out_depth)
+def autoformat_filter_conv3d(fsize, in_depth, out_depth):
+    if isinstance(fsize, int):
+        return [fsize, fsize, fsize, in_depth, out_depth]
+    elif isinstance(fsize, (tuple, list)):
+        if len(fsize) == 3:
+            return [fsize[0], fsize[1],fsize[2], in_depth, out_depth]
+        else:
+            raise Exception("filter length error: " + str(len(fsize))
+                            + ", only a length of 3 is supported.")
+    else:
+        raise Exception("filter format error: " + str(type(fsize)))
+
+
+# Auto format stride for 3d convolution
+def autoformat_stride_3d(strides):
+    if isinstance(strides, int):
+        return [1, strides, strides, strides, 1]
+    elif isinstance(strides, (tuple, list)):
+        if len(strides) == 3:
+            return [1, strides[0], strides[1],strides[2], 1]
+        elif len(strides) == 5:
+            assert strides[0] == strides[4] == 1, "Must have strides[0] = strides[4] = 1"
+            return [strides[0], strides[1], strides[2], strides[3], strides[4]]
+        else:
+            raise Exception("strides length error: " + str(len(strides))
+                            + ", only a length of 3 or 5 is supported.")
+    else:
+        raise Exception("strides format error: " + str(type(strides)))
+
+
+# Auto format kernel for 3d convolution
+def autoformat_kernel_3d(kernel):
+    if isinstance(kernel, int):
+        return [1, 1, kernel, kernel, kernel]
+    elif isinstance(kernel, (tuple, list)):
+        if len(kernel) == 3:
+            return [1, 1, kernel[0], kernel[1], kernel[2]]
+        elif len(kernel) == 5:
+            assert kernel[0] == kernel[1] == 1, "Must have kernel_size[0] = kernel_size[1] = 1"
+            return [kernel[0], kernel[1], kernel[2], kernel[3], kernel[4]]
+        else:
+            raise Exception("kernel length error: " + str(len(kernel))
+                            + ", only a length of 3 or 5 is supported.")
+    else:
+        raise Exception("kernel format error: " + str(type(kernel)))
