c1af0ec34df121ac156176b46fe2d34a8b245d63
aymericdamien <aymeric.damien@gmail.com>
2016-08-22 13:12:40 +0100

    fix #277

--- tflearn/__init__.py
+++ tflearn/__init__.py
@@ -43,7 +43,7 @@ from . import layers
 from .layers.conv import conv_2d, max_pool_2d, avg_pool_2d, conv_1d, \
     highway_conv_2d, highway_conv_1d, max_pool_1d, avg_pool_1d, \
     global_avg_pool, residual_block, residual_bottleneck, \
-    conv_2d_transpose, upsample_2d
+    conv_2d_transpose, upsample_2d, conv_3d, max_pool_3d, avg_pool_3d
 from .layers.core import input_data, dropout, custom_layer, reshape, \
     flatten, activation, fully_connected, single_unit, highway, \
     one_hot_encoding, time_distributed
