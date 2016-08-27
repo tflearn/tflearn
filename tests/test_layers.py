862328d04b0608f4b3d772a381023289d0acc9ed
ichuang <ichuang@mit.edu>
2016-08-27 11:20:52 -0400

    add tests for feed_dict None and placeholder duplicate avoidance

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -98,5 +98,53 @@ class TestLayers(unittest.TestCase):
             m.fit(X, Y, n_epoch=300, snapshot_epoch=False)
             self.assertGreater(m.predict([[5, 9, 11, 1]])[0][1], 0.9)
 
+    def test_regression_placeholder(self):
+        '''
+        Check that regression does not duplicate placeholders
+        '''
+
+        with tf.Graph().as_default():
+
+            g = tflearn.input_data(shape=[None, 2])
+            g_nand = tflearn.fully_connected(g, 1, activation='linear')
+            with tf.name_scope("Y"):
+                Y_in = tf.placeholder(shape=[None, 1], dtype=tf.float32, name="Y")
+            tflearn.regression(g_nand, optimizer='sgd',
+                               placeholder=Y_in,
+                               learning_rate=2.,
+                               loss='binary_crossentropy', 
+                               op_name="regression1",
+                               name="Y")
+            # for this test, just use the same default trainable_vars
+            # in practice, this should be different for the two regressions
+            tflearn.regression(g_nand, optimizer='adam',
+                               placeholder=Y_in,
+                               learning_rate=2.,
+                               loss='binary_crossentropy', 
+                               op_name="regression2",
+                               name="Y")
+
+            self.assertEqual(len(tf.get_collection(tf.GraphKeys.TARGETS)), 1)
+
+    def test_feed_dict_no_None(self):
+
+        X = [[0., 0., 0., 0.], [1., 1., 1., 1.], [0., 0., 1., 0.], [1., 1., 1., 0.]]
+        Y = [[1., 0.], [0., 1.], [1., 0.], [0., 1.]]
+
+        with tf.Graph().as_default():
+            g = tflearn.input_data(shape=[None, 4], name="X_in")
+            g = tflearn.reshape(g, new_shape=[-1, 2, 2, 1])
+            g = tflearn.conv_2d(g, 4, 2)
+            g = tflearn.conv_2d(g, 4, 1)
+            g = tflearn.max_pool_2d(g, 2)
+            g = tflearn.fully_connected(g, 2, activation='softmax')
+            g = tflearn.regression(g, optimizer='sgd', learning_rate=1.)
+
+            m = tflearn.DNN(g)
+
+            def do_fit():
+                m.fit({"X_in": X, 'non_existent': X}, Y, n_epoch=30, snapshot_epoch=False)
+            self.assertRaisesRegexp(Exception, "Feed dict asks for variable named 'non_existent' but no such variable is known to exist", do_fit)
+
 if __name__ == "__main__":
     unittest.main()
