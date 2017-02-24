a8114136936dfa63ca7948e6a88fd3cd1c5b85fa
Robin Kobus <robin.kobus@web.de>
2017-02-24 12:20:39 +0100

    Faster 'to_categorical'
    Numpy integer array indexing is much faster

--- tflearn/data_utils.py
+++ tflearn/data_utils.py
@@ -39,8 +39,7 @@ def to_categorical(y, nb_classes):
     if not nb_classes:
         nb_classes = np.max(y)+1
     Y = np.zeros((len(y), nb_classes))
-    for i in range(len(y)):
-        Y[i, y[i]] = 1.
+    Y[np.arange(len(y)),y] = 1..
     return Y
 
 
