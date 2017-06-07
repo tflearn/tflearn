59b910413e0eae69956f5294a790b6f149d04740
Eduardo Pinho <enet4mikeenet@gmail.com>
2017-06-07 18:55:33 +0100

    Add tests for leaky_relu

--- tests/test.py
+++ tests/test.py
@@ -39,5 +39,25 @@ class TestActivations(unittest.TestCase):
             self.assertAlmostEqual(sess.run(f(x), feed_dict={x:-0.25}),
                 -0.2449, places=TestActivations.PLACES)
 
+    def test_leaky_relu(self):
+        f = lambda x: tflearn.leaky_relu(x, alpha=0.2)
+        x = tf.placeholder(tf.float32, shape=())
+
+        with tf.Session() as sess:
+            # Case 1
+            self.assertEqual(sess.run(f(x), feed_dict={x:0}), 0)
+
+            # Case 2
+            self.assertAlmostEqual(sess.run(f(x), feed_dict={x:1}),
+                1, places=TestActivations.PLACES)
+
+            # Case 3
+            self.assertAlmostEqual(sess.run(f(x), feed_dict={x:-1}),
+                -0.2, places=TestActivations.PLACES)
+
+            # Case 4
+            self.assertAlmostEqual(sess.run(f(x), feed_dict={x:-5}),
+                -1, places=TestActivations.PLACES)
+
 if __name__ == "__main__":
     unittest.main()
