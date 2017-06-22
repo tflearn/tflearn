8af77b5aebcb8aba0f1b855201aed732906c6de8
Eduardo Pinho <enet4mikeenet@gmail.com>
2017-06-22 17:47:18 +0100

    Fix tflearn.activation on callable object (#805)

--- tests/test.py
+++ tests/test.py
@@ -59,5 +59,35 @@ class TestActivations(unittest.TestCase):
             self.assertAlmostEqual(sess.run(f(x), feed_dict={x:-5}),
                 -1, places=TestActivations.PLACES)
 
+    def test_apply_activation(self):
+        lrelu_02 = lambda x: tflearn.leaky_relu(x, alpha=0.2)
+        x = tf.constant(-0.25, tf.float32)
+
+        with tf.Session() as sess:
+            # Case 1: 'linear'
+            self.assertEqual(
+                sess.run(tflearn.activation(x, 'linear')),
+                -0.25)
+
+            # Case 2: 'relu'
+            self.assertEqual(
+                sess.run(tflearn.activation(x, 'relu')),
+                0)
+
+            # Case 3: 'leaky_relu'
+            self.assertAlmostEqual(
+                sess.run(tflearn.activation(x, 'leaky_relu')),
+                -0.025, places=TestActivations.PLACES)
+
+            # Case 4: 'tanh'
+            self.assertAlmostEqual(
+                sess.run(tflearn.activation(x, 'tanh')),
+                -0.2449, places=TestActivations.PLACES)
+
+            # Case 5: lrelu_02 (callable)
+            self.assertAlmostEqual(
+                sess.run(tflearn.activation(x, lrelu_02)),
+                -0.05, places=TestActivations.PLACES)
+
 if __name__ == "__main__":
     unittest.main()
