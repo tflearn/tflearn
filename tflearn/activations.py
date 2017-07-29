d28030a59f965c0f433fe404599436eb9ed902d8
zhengyao jiang <jzyjiangzhengyao@gmail.com>
2017-07-29 20:53:13 +0800

    fix elu activation function (#853)

--- tflearn/activations.py
+++ tflearn/activations.py
@@ -303,4 +303,4 @@ def selu(x):
     """
     alpha = 1.6732632423543772848170429916717
     scale = 1.0507009873554804934193349852946
-    return scale * tf.nn.elu(x, alpha)
+    return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
