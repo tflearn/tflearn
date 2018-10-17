fa93b40058f9bfe5a89217d52bb7303d39e770d1
Aymeric Damien <aymeric.damien@gmail.com>
2018-10-16 20:49:06 -0700

    Update SELU (#1092)

--- tflearn/activations.py
+++ tflearn/activations.py
@@ -301,6 +301,4 @@ def selu(x):
         [https://arxiv.org/abs/1706.02515](https://arxiv.org/abs/1706.02515)
 
     """
-    alpha = 1.6732632423543772848170429916717
-    scale = 1.0507009873554804934193349852946
-    return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
+    return tf.nn.selu(x)
